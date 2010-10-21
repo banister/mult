a78ef137f8b755c6733dd59d954d93b0121d8810
John Mair <jrmair@gmail.com>
2010-10-22 02:44:47 +1300

    added m_tbl stuff and raw ivar setting, version 0.2.0

--- ext/mult/compat.h
+++ ext/mult/compat.h
@@ -5,21 +5,17 @@
 
 #include <ruby.h>
 
-/* this is the test we use to identify ruby 1.9.1 */
-
-/* commenting out this test because it doesn't seem to work in 1.8.7 */
-/*
-#ifdef RCLASS_M_TBL
-# define RUBY_19
-#endif
-*/
-
 /* macros for backwards compatibility with 1.8 */
-#ifndef RCLASS_M_TBL
+#ifndef RUBY_19
 # define RCLASS_M_TBL(c) (RCLASS(c)->m_tbl)
 # define RCLASS_SUPER(c) (RCLASS(c)->super)
 # define RCLASS_IV_TBL(c) (RCLASS(c)->iv_tbl)
+# define OBJ_UNTRUSTED OBJ_TAINTED
+# include "st.h"
 #endif
+
+# define FALSE 0
+# define TRUE 1
 
 /* a useful macro. cannot use ordinary CLASS_OF as it does not return an lvalue */
 #define KLASS_OF(c) (RBASIC(c)->klass)
--- ext/mult/mult.c
+++ ext/mult/mult.c
@@ -1,17 +1,23 @@
 #include <ruby.h>
 #include "compat.h"
 
+static VALUE cRubyTable;
+
+typedef struct {
+  st_table* table;
+} RubyTable;
+
 /* return the actual class of an object */
 static VALUE
 get_class(VALUE self) {    
-    return CLASS_OF(self);
+  return CLASS_OF(self);
 }
 
 /* set the class of an object */
 static VALUE
 set_class(VALUE self, VALUE klass) {
-    KLASS_OF(self) = klass;
-    return klass;
+  KLASS_OF(self) = klass;
+  return klass;
 }
 
 
@@ -19,54 +25,105 @@ set_class(VALUE self, VALUE klass) {
 static VALUE
 get_super(VALUE self)
 {
-    return RCLASS_SUPER(self);
+  return RCLASS_SUPER(self);
 }
 
 /* set the super class of an object */
 static VALUE
 set_super(VALUE self, VALUE klass) {    
-    RCLASS_SUPER(self) = klass;
-    return klass;
+  RCLASS_SUPER(self) = klass;
+  return klass;
 }
 
 /* is the object a singleton class ? */
 static VALUE
 is_singleton(VALUE self, VALUE klass) {    
-    return FL_TEST(self, FL_SINGLETON) ? Qtrue : Qfalse;
+  return FL_TEST(self, FL_SINGLETON) ? Qtrue : Qfalse;
+}
+
+/* get a raw instance var */
+static VALUE
+get_ivar(VALUE self, VALUE sym)
+{
+  return rb_ivar_get(self, rb_to_id(sym));
+}
+
+/* set a raw instance var */
+static VALUE
+set_ivar(VALUE self, VALUE sym, VALUE val)
+{
+  return rb_ivar_set(self, rb_to_id(sym), val);
 }
 
 /* get the attached class if receiver is a singleton class */
 static VALUE
 get_attached(VALUE self)
 {
-    if(FL_TEST(self, FL_SINGLETON))
-        return rb_iv_get(self, "__attached__");
-    else
-        return Qnil;
+  if(FL_TEST(self, FL_SINGLETON))
+    return rb_iv_get(self, "__attached__");
+  else
+    return Qnil;
 }
-
 
 /* set the attached class if receiver is a singleton class */
 static VALUE
 set_attached(VALUE self, VALUE klass) { 
-    if(FL_TEST(self, FL_SINGLETON)) { 
-        rb_iv_set(self, "__attached__", klass);
-        return self;
-    }
-    else
-        return Qnil;
+  if(FL_TEST(self, FL_SINGLETON)) { 
+    rb_iv_set(self, "__attached__", klass);
+    return self;
+  }
+  else
+    return Qnil;
+}
+
+/* get the method table */
+static VALUE
+get_m_tbl(VALUE self) {
+  RubyTable * m_tbl_struct = ALLOC(RubyTable);
+
+  if (!RCLASS_M_TBL(self))
+    RCLASS_M_TBL(self) = st_init_numtable();
+
+  m_tbl_struct->table = RCLASS_M_TBL(self);
+  VALUE ret = Data_Wrap_Struct(cRubyTable, 0, free, m_tbl_struct);
+
+  return ret;
+}
+
+/* set the method table */
+static VALUE
+set_m_tbl(VALUE self, VALUE rb_m) {
+  Check_Type(rb_m, T_DATA);
+  
+  RubyTable * m_tbl_struct;
+  Data_Get_Struct(rb_m, RubyTable, m_tbl_struct);
+  
+  RCLASS_M_TBL(self) = m_tbl_struct->table;
+
+  rb_clear_cache();
+  
+  return self;
 }
 
 void 
 Init_mult() {
-    rb_define_method(rb_cObject, "actual_class", get_class, 0);
-    rb_define_method(rb_cObject, "actual_class=", set_class, 1);
 
-    rb_define_method(rb_cModule, "actual_super", get_super, 0);
-    rb_define_method(rb_cModule, "actual_super=", set_super, 1);
+  cRubyTable = rb_define_class("RubyTable", rb_cObject);
+  
+  rb_define_method(rb_cObject, "actual_class", get_class, 0);
+  rb_define_method(rb_cObject, "actual_class=", set_class, 1);
 
-    rb_define_method(rb_cClass, "is_singleton?", is_singleton, 0);
+  rb_define_method(rb_cObject, "ivar_get", get_ivar, 1);
+  rb_define_method(rb_cObject, "ivar_set", set_ivar, 2);
+
+  rb_define_method(rb_cModule, "m_tbl", get_m_tbl, 0);
+  rb_define_method(rb_cModule, "m_tbl=", set_m_tbl, 1);
+
+  rb_define_method(rb_cModule, "actual_super", get_super, 0);
+  rb_define_method(rb_cModule, "actual_super=", set_super, 1);
+
+  rb_define_method(rb_cClass, "is_singleton?", is_singleton, 0);
     
-    rb_define_method(rb_cClass, "attached", get_attached, 0);
-    rb_define_method(rb_cClass, "attached=", set_attached, 1);
+  rb_define_method(rb_cClass, "attached", get_attached, 0);
+  rb_define_method(rb_cClass, "attached=", set_attached, 1);
 }
